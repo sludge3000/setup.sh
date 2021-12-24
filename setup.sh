7684c1361165a9cb9b3460268be5080bdd9418ec
sludge3000 <9828190+sludge3000@users.noreply.github.com>
2021-12-24 15:44:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ fi
 # GIT clone stuff
 git clone https://github.com/sludge3000/dot-files
 git clone https://github.com/AlessandroYorba/Alduin
+git clone https://github.com/junegunn/seoul256.vim
+git clone https://github.com/nanotech/jellybeans.vim
 
 # Check if already in $HOME and move stuff
 DIRECTORY=$(cd `dirname $0` && pwd)
@@ -21,12 +23,17 @@ if [ $DIRECTORY != $HOME ];
         cp dot-files/.vimrc ~/.vimrc
         cp dot-files/.tmux.conf ~/.tmux.conf
         cp dot-files/.bashrc ~/.bashrc
+        cp dot-files/.bash_aliases ~/.bash_aliases
         cp -r dot-files/.git ~/.git
 fi
 
 # Copy VIM color schemes to correct location
 cp Alduin/colors/alduin.vim ~/.vim/colors/alduin.vim
+cp seoul256/colors/seoul256.cim ~/.vim/colors/seoul256.vim
+cp jellybeans/colors/jellybeans.vim ~/.vim/colors/jellybeans.vim
 
 # Tidy up
-rm -rf dot-files
-rm -rf Alduin
+rm -r dot-files
+rm -r Alduin
+rm -r seoul256
+rm -r jellybeans
