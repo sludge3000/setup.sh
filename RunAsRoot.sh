b77ca7999b5c671c6e34bad72f0ebfea76b81279
sludge3000 <9828190+sludge3000@users.noreply.github.com>
2021-12-24 16:25:28 +0100

    Update RunAsRoot.sh

--- RunAsRoot.sh
+++ RunAsRoot.sh
@@ -1,6 +1,16 @@
 # The following must be run as root.
 # Writen for Debian11
 #
+#
+# Install things I use regularly
+# Assumes git already installed
+#
+apt -y -q update
+apt -y -q upgrade
+apt -y -q install vim tmux tcpdump fortune-mod cowsay lolcat wireshark pipenv python3-scapy
+
+#
+#
 # For scapy
 cp /usr/bin/python3 /home/motoko/.python39_netraw
 chown motoko /home/motoko/.python39_netraw
